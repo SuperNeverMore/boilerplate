2c2ec0bbb734adbe28518c97f73e212ac4856b25
SuperNever <1021165525@qq.com>
2017-10-27 11:01:51 +0800

    修改参数

--- command/init.js
+++ command/init.js
@@ -7,18 +7,22 @@ const chalk = require('chalk')
 
 module.exports =()=>{
 	co(function*(){
-		let tplName = yield prompt('模板名称为: ');
 		let projectName = yield prompt('项目名称为: ')
+		let tplName = yield prompt('模板名称为: ');
 		let gitUrl;
 		let branch;
-
-		if(!config.template[tplName]){
-			console.log(chalk.red('\n 模板不存在'))
-			process.exit();
+		if (tplName) {
+			if(!config.template[tplName]){
+				console.log(chalk.red('\n 模板不存在'))
+				process.exit();
+			} else {
+				gitUrl = config.template[tplName].url;
+				branch = config.template[tplName].branch;
+			}
+		} else {
+			gitUrl = config.template['website'].url;
+			branch = config.template['website'].branch;
 		}
-		gitUrl = config.template[tplName].url;
-		branch = config.template[tplName].branch;
-
 		let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`;
 		console.log(chalk.white('\n 开始构建...'))
 		exec(cmdStr,(error,stdout,stderr)=>{
